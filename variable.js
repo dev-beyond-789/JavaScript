923ad1e249143cec0026720225f2aa1d015e29ff
dev-beyond-789 <sarthak1110000@gmail.com>
2026-05-21 18:58:31 +0545

    feat :variable

---
+++ variable.js
@@ -0,0 +1,22 @@
+const PI = 3.14;
+// console.log(PI); //3.14
+// PI = 4;
+// console.log(PI);
+
+let a = 7;
+// console.log(a);
+a = 8;
+// console.log(a);
+
+{
+  var m = 7;
+  console.log(m);
+}
+console.log(m);
+
+
+{
+  let n = 9
+  console.log(n);
+}
+console.log(n)
